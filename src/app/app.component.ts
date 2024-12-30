ee902547d15b0e3ad7359cc95e97a0c6b6bf8061
PhysicsIsLove <aa.iitbbs@gmail.com>
2024-12-31 01:03:57 +0530

    added svg icons and message sending functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,51 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
+import { EMAIL_JS_PUBLIC_KEY, EMAIL_JS_SERVICE_ID, EMAIL_JS_TEMPLATE_ID } from './constants';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+
+  contactForm: FormGroup;
+  
   title = 'my-portfolio';
+
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      from_name: ['', [Validators.required]],
+      email: ['', [Validators.email, Validators.required]],
+      message: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      console.log(this.contactForm.value);
+    }
+  }
+
+  public sendEmail(e: Event) {
+    debugger
+    e.preventDefault();
+    if (this.contactForm.valid) {
+      emailjs.send(EMAIL_JS_SERVICE_ID, EMAIL_JS_TEMPLATE_ID, this.contactForm.value, EMAIL_JS_PUBLIC_KEY)
+        .then((result: EmailJSResponseStatus) => {
+          console.log(result.text);
+          alert('Message sent successfully!');
+          this.contactForm.reset();
+        }, (error) => {
+          console.log(error.text);
+          alert('Failed to send message. Please try again later.');
+        });
+    }
+  }
+
 }
---
+++ src/app/constants.ts
@@ -0,0 +1,3 @@
+export const EMAIL_JS_SERVICE_ID = "my_portfolio"
+export const EMAIL_JS_TEMPLATE_ID = "template_69715mm"
+export const EMAIL_JS_PUBLIC_KEY = "_HQSCHYyW15rXUgdA"
